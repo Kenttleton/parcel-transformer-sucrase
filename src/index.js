b38c8c5a25c85c513e131e9fa2ec7b649a305b51
Kent <utterback.kent.a@gmail.com>
2021-04-22 01:12:42 -0700

    Add flow for V2

---
+++ src/index.js
@@ -0,0 +1,21 @@
+const { Transformer } = require("@parcel/plugin");
+const { transform } = require("sucrase");
+
+module.exports = new Transformer({
+    async parse({asset}) {
+        console.log(asset);
+        //TODO Parse file type and add to transforms
+        return {transforms: ["jsx", "typescript", "flow",  "imports", "react-hot-loader", "jest"]}; // All available transforms from Sucrase
+    },
+
+    async transform({asset}) {
+        let options = this.parse({asset});
+        return transform(await asset.getCode(), options);
+    },
+
+    async generate({asset}){
+        const compiled = await this.transform({asset});
+        return { code: compiled.code, map: compiled.sourceMap };
+    }
+});
+
